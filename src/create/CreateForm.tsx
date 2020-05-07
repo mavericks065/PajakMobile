4cd604b8ec8209661caa63c2da5ba4456c84858a
nicolas <nicolas065@gmail.com>
2020-05-07 12:39:02 +1000

    add to the model information

--- src/create/CreateForm.tsx
+++ src/create/CreateForm.tsx
@@ -1,3 +1,11 @@
 import React from "react";
 import {Text, View} from "react-native";
+import {Invoice} from "../model/Model";
 
+const CreateForm :React.FC<Invoice> = (invoice) => {
+    return (
+        <Text>Create Form</Text>
+    );
+}
+
+export default CreateForm;
--- src/model/Model.ts
+++ src/model/Model.ts
@@ -2,8 +2,29 @@ import React from "react";
 import {Text, View} from "react-native";
 
 export interface Invoice {
-    vendor: string;
-    amount: number;
-    taxPeriod: string;
-    date: string;
+    vendor?: string; // to be removed and replaced by CounterPart
+    amount?: number; // to be removed and replaced by unitAmount
+    taxPeriod?: string;
+    date?: string; // to be removed and replaced by invoiceDate and creationDate
+    counterPart?: CounterPart;
+    creationDate: Date;
+    invoiceDate?: Date;
+    dueDate?: Date;
+    unitNumber?: number;
+    unitAmount?: number;
+    discount?: number;
+    goods?: string;
+    notes?: string;
+    // to add file after for the picture
+}
+
+export interface CounterPart {
+    isBuyer?: Boolean;
+    email?: string;
+    address?: string;
+    postCode?: string;
+    city?: string;
+    name?: string;
+    paymentDetails?: string;
+    npwp?: string;
 }
